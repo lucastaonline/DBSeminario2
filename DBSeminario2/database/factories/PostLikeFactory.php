37bf33ff7492ab2c46789c49ea08868fb7fd4e99
1uuc4asb <1uuc4asb@gmail.com>
2019-06-25 00:43:08 -0300

    Corrigindo factory PostLike

--- DBSeminario2/database/factories/PostLikeFactory.php
+++ DBSeminario2/database/factories/PostLikeFactory.php
@@ -14,12 +14,14 @@ $factory->define(App\LikePost::class, function (Faker $faker) {
     while(!$conseguiuAchar && $limitador < 100000) {
         $accountId = Account::inRandomOrder()->first()->id;
         $postId = PostSql::inRandomOrder()->first()->id;
-        if(LikePost::where('account_id', $accountId)->where('post_id', $postId)->count() > 0)
+        if(LikePost::where('account_id', $accountId)->where('post_id', $postId)->count() == 0)
             $conseguiuAchar = true;
         $limitador += 1;
     }
-    return [
-        'account_id' => $accountId,
-        'post_id' => $postId,
-    ];
+    if($conseguiuAchar) {
+        return [
+            'account_id' => $accountId,
+            'post_id' => $postId,
+        ];
+    }
 });
